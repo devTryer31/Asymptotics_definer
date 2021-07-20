f8bb3aba17094ccdf3f07f5c15601dc669630807
Валера Герасимов <valera.gs@bk.ru>
2021-07-20 16:30:02 +0300

    Base infrastructure implemented.

---
+++ ViewModels/Base/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Asymptotics_definer.ViewModels.Base {
+	internal abstract class ViewModel : INotifyPropertyChanged {
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+			PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(propertyName));
+		}
+
+		protected virtual bool Set<T>(ref T source, T value, [CallerMemberName] string propertyName = null) {
+			if (object.Equals(source, value))
+				return true;
+
+			source = value;
+			OnPropertyChanged(propertyName);
+			return true;
+		}
+	}
+}
---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Asymptotics_definer.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Asymptotics_definer.ViewModels {
+	internal sealed class MainWindowViewModel : ViewModel{
+	
+	
+	}
+}
