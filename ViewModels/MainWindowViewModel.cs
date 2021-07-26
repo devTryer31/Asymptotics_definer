975c0768b340bc9c3adc98241e498a6e7befab6d
Валера Герасимов <valera.gs@bk.ru>
2021-07-26 23:08:37 +0300

    Processed data plotting done.

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Asymptotics_definer.Infrastructure.Commands;
+using Asymptotics_definer.Models;
 using Asymptotics_definer.ViewModels.Base;
 using Microsoft.Win32;
 using System;
@@ -36,6 +37,28 @@ namespace Asymptotics_definer.ViewModels {
 
 		#endregion
 
+		#region MainGraphComputedPoints : ObservableCollection<KeyValuePair<int, double>>
+
+		private ObservableCollection<KeyValuePair<uint, double>> _MainGraphComputedPoints;
+
+		public ObservableCollection<KeyValuePair<uint, double>> MainGraphComputedPoints {
+			get => _MainGraphComputedPoints;
+			set => Set(ref _MainGraphComputedPoints, value);
+		}
+
+		#endregion
+
+		#region ResultPlotTitle
+
+		private string _ResultPlotTitle;
+
+		public string ResultPlotTitle {
+			get => _ResultPlotTitle;
+			set => Set(ref _ResultPlotTitle, value);
+		}
+
+
+		#endregion
 
 		#region Commands
 
@@ -43,7 +66,7 @@ namespace Asymptotics_definer.ViewModels {
 
 		public ICommand OpenFileCommand { get; }
 
-		private bool CanOpenFileCommandExecute(object param) => true; //TODO: When computing => false.
+		private bool CanOpenFileCommandExecute(object param) => true; //TODO: When computing => false. Async method require.
 
 		private void OnOpenFileCommandExecuted(object patam) {
 			OpenFileDialog openFileDialog = new OpenFileDialog() {
@@ -64,15 +87,37 @@ namespace Asymptotics_definer.ViewModels {
 					return;
 				}
 				else {
-					List<KeyValuePair<uint, double>> pointsList= new List<KeyValuePair<uint, double>>();
+					List<KeyValuePair<uint, double>> pointsList = new List<KeyValuePair<uint, double>>();
 					while (!sr.EndOfStream) {
 						pointsList.Add(new KeyValuePair<uint, double>(uint.Parse(line[0]), double.Parse(line[1])));
 						line = sr.ReadLine().Split(';', ',', ' ');
 					}
+					pointsList.Add(new KeyValuePair<uint, double>(uint.Parse(line[0]), double.Parse(line[1])));
 					MainGraphPoints = new ObservableCollection<KeyValuePair<uint, double>>(
 						pointsList);
 				}
 			}
+		}
+
+		#endregion
+
+		#region ComputeCommand
+
+		public ICommand ComputeCommand { get; }
+
+		private bool CanComputeCommandExecute(object param) => MainGraphPoints != null && MainGraphPoints.Count != 0;
+
+		private void OnComputeCommandExecuted(object param) {
+			var res = AsymptoticsDefiner.Evaluate(
+				new Dictionary<int, int>(
+					MainGraphPoints.Select(x => new KeyValuePair<int, int>((int)x.Key, (int)x.Value )))
+				);
+			var lst = new List<KeyValuePair<uint, double>>(MainGraphPoints.Count);
+			foreach (var p in MainGraphPoints) {
+				lst.Add(new KeyValuePair<uint, double>(p.Key, res.a * res.FuncItem.Exec(p.Key)));
+			}
+			MainGraphComputedPoints = new ObservableCollection<KeyValuePair<uint, double>>(lst);
+			ResultPlotTitle = "Rn(N) = "+res.a.ToString() + res.FuncItem.Str;
 		}
 
 		#endregion
@@ -85,6 +130,8 @@ namespace Asymptotics_definer.ViewModels {
 			#region Commands
 
 			OpenFileCommand = new LambdaCommand(OnOpenFileCommandExecuted, CanOpenFileCommandExecute);
+
+			ComputeCommand = new LambdaCommand(OnComputeCommandExecuted, CanComputeCommandExecute);
 
 			#endregion
 
