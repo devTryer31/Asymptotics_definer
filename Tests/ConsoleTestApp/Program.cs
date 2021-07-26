905a9667c17219ac4bc5244abaf7e88632d112a1
Валера Герасимов <valera.gs@bk.ru>
2021-07-26 15:27:20 +0300

    Attempt to use command object like a _ImportedFile modificator.

--- Tests/ConsoleTestApp/Program.cs
+++ Tests/ConsoleTestApp/Program.cs
@@ -1,50 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleTestApp {
 	class Program {
 		static void Main(string[] args) {
 			System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
 
+			FileInfo fileInfo = null;
+			Console.WriteLine(fileInfo is FileInfo );
 
-			Console.WriteLine("Tests: ");
-			//From seminar before 2nd test.
-			var res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
-				{1, 0},
-				{2, 0},
-				{3, 1},
-				{4, 5},
-				{5, 10},
-				{6, 22},
-				{7, 25},
-				{8, 50},
-				{9, 99}
-			});
 
-			Console.WriteLine($"O1(N) = {res.a}{res.FuncItem.Str}");
+			//Console.WriteLine("Tests: ");
+			////From seminar before 2nd test.
+			//var res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
+			//	{1, 0},
+			//	{2, 0},
+			//	{3, 1},
+			//	{4, 5},
+			//	{5, 10},
+			//	{6, 22},
+			//	{7, 25},
+			//	{8, 50},
+			//	{9, 99}
+			//});
 
-			res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
-				{300,8 },
-				{1000,9 },
-				{3000,11 },
-				{10000,13 },
-				{30000,14 },
-				{50000,15 },
-			});
-			Console.WriteLine($"O2(N) = {res.a}{res.FuncItem.Str}");
+			//Console.WriteLine($"O1(N) = {res.a}{res.FuncItem.Str}");
 
-			res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
-				{1, 1},
-				{2, 2},
-				{3, 3},
-				{4, 3},
-				{5, 5},
-				{6, 6},
-				{7, 7},
-				{8, 8},
-				{9, 9}
-			});
-			Console.WriteLine($"O3(N) = {res.a}{res.FuncItem.Str}");
+			//res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
+			//	{300,8 },
+			//	{1000,9 },
+			//	{3000,11 },
+			//	{10000,13 },
+			//	{30000,14 },
+			//	{50000,15 },
+			//});
+			//Console.WriteLine($"O2(N) = {res.a}{res.FuncItem.Str}");
+
+			//res = AsymptoticsDefiner.Evaluate(new Dictionary<int, int>() {
+			//	{1, 1},
+			//	{2, 2},
+			//	{3, 3},
+			//	{4, 3},
+			//	{5, 5},
+			//	{6, 6},
+			//	{7, 7},
+			//	{8, 8},
+			//	{9, 9}
+			//});
+			//Console.WriteLine($"O3(N) = {res.a}{res.FuncItem.Str}");
 
 		}
 	}
--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,24 @@
 ﻿using Asymptotics_definer.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Asymptotics_definer.ViewModels {
 
-	internal sealed class MainWindowViewModel : ViewModel{
-	
-	
+	internal sealed class MainWindowViewModel : ViewModel {
+
+		#region ImportedFile : FileInfo
+
+		private FileInfo _ImportedFile;
+
+		public FileInfo ImportedFile {
+			get => _ImportedFile;
+			set => Set(ref _ImportedFile, value);
+		}
+
+		#endregion
 	}
 }
